8297cb2f678258250d0897b87bbc25d8eb2abe9b
alexdelicato <adel0011@student.monash.edu>
2022-09-08 09:48:18 +1000

    calculates the scenario demand for a set of demand features

--- parse_dataset_csv.py
+++ parse_dataset_csv.py
@@ -1,7 +1,6 @@
 # code to parse and analyse features from DataSetfeatures file
 # *note: assumes file 'DataSetFeatures.csv' is in same folder
 import pandas as pd
-import math
 
 # we want to map the information in each row to a dictionary
 # whose keys are are given by a fieldnames parameter
@@ -9,11 +8,10 @@ data = pd.read_csv('DataSetfeatures.csv')
 
 
 # calculates demand value for a given scenario
-def scenario_demand(overall_data, feature_name):
-    scenario = overall_data.iloc[0]
-    scenario_id = scenario['feature_scenarioIdentifier']
+def feature_demand(scenario, overall_data, feature_name):
+    # Returns 1 if high demand feature value
+    # Returns 0 if not high demand feature value
 
-    print('calculating demand for scenario ' + str(scenario_id))
     # find min demand for feature
     feature_min = min(overall_data[feature_name])
     print('min: '+str(feature_min))
@@ -28,9 +26,36 @@ def scenario_demand(overall_data, feature_name):
     print('medium: '+str(feature_medium))
     print('value: '+str(feature_value))
     if feature_value > feature_medium:
-        print(str(feature_name) + ' is demanding for scenario ' + str(scenario_id))
+        print(str(feature_name) + ' is demanding for scenario ')
+        return 1
     else:  # if < medium then low demand
-        print(str(feature_name) + ' is not demanding for scenario ' + str(scenario_id))
+        print(str(feature_name) + ' is not demanding for scenario ')
+        return 0
 
 
-scenario_demand(data, 'feature_ego_speedDemand')
+def scenario_demand(scenario_num, overall_data, feature_set):
+    scenario = overall_data.iloc[scenario_num]
+    scenario_id = scenario['feature_scenarioIdentifier']
+    print('calculating demand for scenario ' + str(scenario_id))
+
+    demand_sum = 0
+    # for each feature in the feature set, find whether the value is high demand
+    # add demand value to demand_sum - most demanding scenarios will have highest sum
+    for feature in feature_set:
+        print('\nfinding demand for feature '+str(feature))
+        demand_sum += feature_demand(scenario, overall_data, feature)
+
+    print('\nOverall scenario demand is '+str(demand_sum))
+
+
+demand_features = ['feature_ego_speedDemand', 'feature_rainDemand', 'feature_fogDemand', 'feature_wetnessDemand',
+                   'feature_timeDemand', 'feature_scenarioTrafficLightDemand', 'feature_scenarioSideWalkDemand']
+scenario_demand(0, data, demand_features)
+# DEMAND FEATURES
+# 'feature_ego_speedDemand'
+# 'feature_rainDemand'
+# 'feature_fogDemand'
+# 'feature_wetnessDemand'
+# 'feature_timeDemand'
+# 'feature_scenarioTrafficLightDemand'
+# 'feature_scenarioSideWalkDemand'
