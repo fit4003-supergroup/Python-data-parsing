0f2c1c02316ce966fc2ec5637df332dc79c32071
Ruby Rizza <rriz6@student.monash.edu>
2022-09-13 15:50:12 +1000

    script to address issue with speed demands

---
+++ speed_demand_fix.py
@@ -0,0 +1,67 @@
+"""
+file to read from data from DataSetfeatures.csv and
+identify and update incorrect speed classifications
+"""
+from msilib.schema import Error
+from selectors import EpollSelector
+import pandas as pd
+
+# define column headings
+speed_heading = 'feature_ego_speed'
+speed_demand_heading = 'feature_ego_speedDemand'
+speed_label_heading = "feature_ego_speedCategory"
+scenario_identifier_heading = "feature_scenarioIdentifier"
+
+class DemandCategory:
+  def __init__(self, demand, label):
+        self.label = label
+        self.demand = demand
+
+# define speed classes       
+stop_max_speed = 0.01
+slow_max_speed = 5
+moderate_max_speed = 8
+        
+class SpeedCategory(DemandCategory):
+  def __init__(self, speed):
+
+        if 0 <= speed <= stop_max_speed:
+            DemandCategory.__init__(self, 1, "Stop (0 < speed (m/s) <= 0.01)")
+            
+        elif stop_max_speed < speed <= slow_max_speed:
+            DemandCategory.__init__(self, 2, "Slow (0.01 < speed (m/s) <= 5)")
+            
+        elif slow_max_speed < speed <= moderate_max_speed:
+            DemandCategory.__init__(self, 3, "Moderate (5 < speed (m/s) <= 8)")
+            
+        elif moderate_max_speed < speed:
+            DemandCategory.__init__(self, 4, "Fast (speed (m/s) > 8)")
+        
+
+
+# read data from csv
+data = pd.read_csv('DataSetfeatures.csv', index_col=0)
+
+# iterate through rows
+for index, row in data.iterrows():
+    
+    # extrect speed and current demand and label
+    speed = row[speed_heading]
+    old_demand = row[speed_demand_heading]
+    old_label = row[speed_label_heading]
+    
+    # generate demand and label
+    category = SpeedCategory(speed)
+    new_demand = category.demand
+    new_label = category.label
+    
+    # update if extracted and generated do not match
+    if old_demand != new_demand or old_label != new_label:
+        print("updating scenario " + str(index))
+        data.loc[index, speed_demand_heading] = new_demand
+        data.loc[index, speed_label_heading] = new_label
+        
+
+print("saving...")
+data.to_csv('DataSetfeatures.csv')
+
